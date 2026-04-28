150b3f83b4f27d0a0284f7bfc3e3dcc6b40d64d5
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-04-28 22:48:49 +0530

    Taking an input script

---
+++ input_basics.py
@@ -0,0 +1,16 @@
+
+# Input
+name = input("Enter your name: ")     # the variable name has the data of the input name
+age = input("Enter your age: ")       # the variable age has the input age
+gender = input("Enter your Gender: ") 
+print("The name is" , name , "and your age is", age, "and gender is", gender)
+
+sum = 0
+for i in range (1,1000):
+    sum = sum + i
+print(sum)
+
+
+
+
+
