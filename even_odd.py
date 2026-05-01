bd25cedabb0514e9c7e7e40acb2b1463bcf05a14
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-05-01 19:41:38 +0530

    Even Odd program

---
+++ even_odd.py
@@ -0,0 +1,13 @@
+# classify between even numbers and odd numbers using if elif statements.
+# modulus (% operator) returns the reminder after division between 2 numbers.
+
+number = int(input("Enter the number: "))
+
+if number%2 == 1:
+    print("The number is Odd")
+
+elif number%2 == 0:
+    print("The number is even")
+
+
+# atanu arnab rituraj rudra
