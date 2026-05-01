0fb53e22ee17b8138a4faf3c5a9fc2b02cf37dbe
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-05-01 19:42:02 +0530

    Even Odd program

--- even_odd.py
+++ even_odd.py
@@ -3,10 +3,10 @@
 
 number = int(input("Enter the number: "))
 
-if number%2 == 1:
+if number%2 == 1:    # number = 15, 15%2 = 1
     print("The number is Odd")
 
-elif number%2 == 0:
+elif number%2 == 0:   # number = 16, 16%2 = 0
     print("The number is even")
 
 
