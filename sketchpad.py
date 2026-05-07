6b917aa91083bdbae301923e0ee76cc44ab1a763
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-05-07 12:21:05 +0530

    A sketchpad for random codes

---
+++ sketchpad.py
@@ -0,0 +1,28 @@
+# len() function gives us the length of the passed variable
+# .append() is used to put new elements in a list 
+#.remove() removes the passed element from the list
+
+#Syntax:    list_name.append(element_to_add) 
+#Syntax:    list_name.remove(element_to_remove)
+#syntax     len(list_name)
+
+
+# number_list = [1,2,3,4,5,6,7,8,9,0]
+
+# #print the square of each number in the list
+# for number in number_list:
+#     print(number**2)
+
+# name = input("Ke Esecho? ")
+# bestfriends_list = ["Sohan", "Sopan", "Argha"]
+# angry_list = ["Rudra", "Rituraj", "Suhan", "Atanu", "Rupayan"]
+# if name not in bestfriends_list and name not in angry_list:
+#     print("Sohan kotha bolbe na")
+
+# elif name in angry_list:
+#     print("Aage mukhe neye tarpor kotha bolbo")
+    
+# elif name in bestfriends_list :
+#     print("Tumi bhalo bondhu, kotha bolbo")
+
+
