17134f9ddbaf5cff708638f0bcac2b52a51c65b0
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-05-26 20:09:08 +0530

    Blah Blah Blah

--- sketchpad.py
+++ sketchpad.py
@@ -1,30 +1,19 @@
-a= int(input("enter the marks for maths"))
-b= int(input("enter the marks for science"))
-c= int(input("enter the marks for bengali"))
-d= int(input("enter the marks for english"))
-e= int(input("enter the marks for IT"))
-f= int(input("enter the marks for SSC"))
-mark_list = [a,b,c,d,e,f]
-worst = min(mark_list)
-mark_list.remove(worst)
-print("Removed: ", worst)
+def traingle_area():
+    base = int(input("Enter the base of the triangle: "))
+    height = int(input("Enter the height of the triangle: "))
+    return 0.5*base*height
 
-if sum(mark_list)/5>90:
-    print("grade a")
+def frustum():
+    a1 = int(input("Enter Lower radius (R): ")) 
+    a2 = int(input("Enter Upper radius (r): ")) 
+    h =  int(input("Enter the height (h): "))   
+    volume = 0.33 * 3.14 * h * (a1**2 + a2**2 + a1*a2)
+    return volume
 
-elif sum(mark_list)/5>80 and sum(mark_list)/5<=90:
-    print("garde b")
-
-elif sum(mark_list)/5>70 and sum(mark_list)/5<=80:
-    print("garde c")
-
-elif sum(mark_list)/5>60 and sum(mark_list)/5<=70:
-    print("garde d")
-
-else:
-    print("Fail bara tui")
-
-
+frustum1 = frustum()
+frustum2 = frustum()
+frustum3 = frustum()
+frustum4 = frustum()
 
 
 
