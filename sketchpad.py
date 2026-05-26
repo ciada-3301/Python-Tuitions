2402ccb90cdf9d5c02c722af1736bc6afeaf079b
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-05-26 18:58:59 +0530

    It is a sketchpad...what do i even write here

--- sketchpad.py
+++ sketchpad.py
@@ -1,28 +1,30 @@
-# len() function gives us the length of the passed variable
-# .append() is used to put new elements in a list 
-#.remove() removes the passed element from the list
+a= int(input("enter the marks for maths"))
+b= int(input("enter the marks for science"))
+c= int(input("enter the marks for bengali"))
+d= int(input("enter the marks for english"))
+e= int(input("enter the marks for IT"))
+f= int(input("enter the marks for SSC"))
+mark_list = [a,b,c,d,e,f]
+worst = min(mark_list)
+mark_list.remove(worst)
+print("Removed: ", worst)
 
-#Syntax:    list_name.append(element_to_add) 
-#Syntax:    list_name.remove(element_to_remove)
-#syntax     len(list_name)
+if sum(mark_list)/5>90:
+    print("grade a")
+
+elif sum(mark_list)/5>80 and sum(mark_list)/5<=90:
+    print("garde b")
+
+elif sum(mark_list)/5>70 and sum(mark_list)/5<=80:
+    print("garde c")
+
+elif sum(mark_list)/5>60 and sum(mark_list)/5<=70:
+    print("garde d")
+
+else:
+    print("Fail bara tui")
 
 
-# number_list = [1,2,3,4,5,6,7,8,9,0]
 
-# #print the square of each number in the list
-# for number in number_list:
-#     print(number**2)
-
-# name = input("Ke Esecho? ")
-# bestfriends_list = ["Sohan", "Sopan", "Argha"]
-# angry_list = ["Rudra", "Rituraj", "Suhan", "Atanu", "Rupayan"]
-# if name not in bestfriends_list and name not in angry_list:
-#     print("Sohan kotha bolbe na")
-
-# elif name in angry_list:
-#     print("Aage mukhe neye tarpor kotha bolbo")
-    
-# elif name in bestfriends_list :
-#     print("Tumi bhalo bondhu, kotha bolbo")
 
 
