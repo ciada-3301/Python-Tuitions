ac7572a631a9c7fc5ea766b6d2ee049526b50209
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-05-28 21:48:23 +0530

    .

--- sketchpad.py
+++ sketchpad.py
@@ -1,19 +1,8 @@
-def traingle_area():
-    base = int(input("Enter the base of the triangle: "))
-    height = int(input("Enter the height of the triangle: "))
-    return 0.5*base*height
+maskera_ship = {"Ship1": {"cont1": "Wheat", "cont2": "Rice", "cont3": "Automobile"},
+                "Ship2": {"cont1": "Wheat", "cont2": "Rice", "cont3": "Automobile"},
+                "Ship3": {"cont1": "Wheat", "cont2": "Rice", "cont3": "Automobile"}}
 
-def frustum():
-    a1 = int(input("Enter Lower radius (R): ")) 
-    a2 = int(input("Enter Upper radius (r): ")) 
-    h =  int(input("Enter the height (h): "))   
-    volume = 0.33 * 3.14 * h * (a1**2 + a2**2 + a1*a2)
-    return volume
 
-frustum1 = frustum()
-frustum2 = frustum()
-frustum3 = frustum()
-frustum4 = frustum()
-
+print(maskera_ship["Ship3"]["cont3"])    
 
 
