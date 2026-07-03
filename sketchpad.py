7c35333a01bb048010d8e9359a505f37bac5a0bd
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-07-03 09:15:15 +0530

    updated

--- sketchpad.py
+++
@@ -1,8 +0,0 @@
-maskera_ship = {"Ship1": {"cont1": "Wheat", "cont2": "Rice", "cont3": "Automobile"},
-                "Ship2": {"cont1": "Wheat", "cont2": "Rice", "cont3": "Automobile"},
-                "Ship3": {"cont1": "Wheat", "cont2": "Rice", "cont3": "Automobile"}}
-
-
-print(maskera_ship["Ship3"]["cont3"])    
-
-
