1d53e1414c8522f88e3cee538a09bbe2e0c11099
Arkadyuti Maiti <arkathecodercoder@gmail.com>
2026-07-06 16:23:00 +0600

    Dictionaries in python lesson

---
+++ Basic Tutorials/dictionaries.py
@@ -0,0 +1,9 @@
+# Dictionaries are data setructures that help you to 
+
+alex = {"Name":"Alex", "Gender": "Male"}      # Initialization, Name and Gender are keys
+steve = {"Name":"Steve", "Gender": "Female"}  # Initialization 
+
+print(alex["Name"])  # Retrieving information from an dictionary
+
+steve["Age"] = 21   # Add new key, value pairs to an existing dictionary
+
